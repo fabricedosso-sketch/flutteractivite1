27ae1d0692f5e1f2bfe6a655d59df0f56f2e4475
fabricedosso-sketch <contact.fabricedosso@gmail.com>
2025-11-01 15:31:08 +0000

    Mise à jour du code pour l'exercice de la semaine avec l'ajout de 4 nouveaux blocs

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class PageAcceuil extends StatelessWidget {
            icon: const Icon(Icons.search, color: Colors.white),)
         ],
       ),
-      // Declaration d'un widget pour l'affichage du contenu principale de l'app avec des propriétés enfants comme une image dans notre cas.
+      // Declaration d'un widget pour l'affichage du contenu principale de l'app avec des propriétés enfants comme une image dans notre cas et les autres blocs de notre page d'acceuil.
       // Le widget body regroupe tous les propriétés du corps principal de l'app
       body:const Column(
         children: [
@@ -69,6 +69,9 @@ class PageAcceuil extends StatelessWidget {
   }
 }
 
+/* Déclaration et initialisation de la classe qui va nous permettre de créer le seconde bloc de la page
+ La classe PartieTitre est un autre Widget Stateless qui représente un bloc de type conteneur. 
+ Elle définit un conteneur contenant un element enfant de type colonne qui englobe aussi deux elements enfanst de type texte. */
 class PartieTitre extends StatelessWidget {
   const PartieTitre({super.key});
 
@@ -76,9 +79,12 @@ class PartieTitre extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      // Creation d'une marge interieur de tous les côtés de l'element
       padding: const EdgeInsets.all(20),
+      // Création d'un element enfant de type colonne avec un alignement horizontalement 
       child: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        // Création de deux éléments enfants de type texte avec une propriété style appliquant une taille et une mise en forme de police.
         children: [
           Text("Bienvenue au Magazine Infos", style: TextStyle(
             fontSize: 25,
@@ -94,13 +100,18 @@ class PartieTitre extends StatelessWidget {
   }
 }
 
+/* Déclaration et initialisation de la classe qui va nous permettre de créer le troisième bloc de la page
+ La classe PartieTexte est un autre Widget Stateless qui représente un bloc de type conteneur. 
+ Elle définit un conteneur contenant un element enfant de type texte avec une propriété style appliquant une taille de police. */
 class PartieTexte extends StatelessWidget {
   const PartieTexte({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      // Ajout d'une marge interieur des côtés horizontaux seulement.
       padding: const EdgeInsets.symmetric(horizontal: 20),
+      // Création d'un element enfant de type texte avec une propriété style appliquant une taille de police.
       child: const Text("Magazine Infos est bien plus qu'un magazine d'informations. C'est votre passerelle vers le monde, une source inestimable de connaissances et d'actualités soigneusement selectionnées pour vous éclairer sur les enjeux mondiaux, la culture, la science, et voir même le divertissement (le jeux).", 
       style: TextStyle(fontSize: 14.0),
       ),
@@ -108,6 +119,10 @@ class PartieTexte extends StatelessWidget {
   }
 }
 
+/* Déclaration et initialisation de la classe qui va nous permettre de créer le quatrième bloc de la page
+ La classe PartieIcone est un autre Widget Stateless qui représente un bloc de type conteneur. 
+ Elle définit un conteneur contenant un element enfant de type ligne qui englobe aussi trois elements enfants de type conteneur
+  qui eux à leur tour contiennent chacun un element enfant. Chaque element enfant contient lui aussi des elements enfants qui sonht de type icone, texte et box avec leur propriétés distinctes. */
 class PartieIcone extends StatelessWidget {
   const PartieIcone({super.key});
 
@@ -115,14 +130,23 @@ class PartieIcone extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(bottom: 10),
+      // Ajout d'une ligne dans son widget parent
       child: Row( 
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly, //// Alignement horizontal des widgets enfants (start, center, end, spaceBetween, etc.)
+        // Alignement horizontal des widgets enfants
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+        // Ajout de trois bloc de conteneur dans son widget parent
         children: [ Container( 
+            // Ajout d'une marge intérieure de 20 pixel de tous les côtés au premier widget enfant 
             padding: const EdgeInsets.all(20),
+            // Ajout d'une colonne qui est le widget enfant du widget parent qui est le premier conteneur
             child: Column(
+              // Création de trois bloc de conteneur dans le widget parent
               children: [
+              // Ajout d'une icone de téléphone avec une couleur rose
               const Icon(Icons.phone, color: Colors.pink),
+              // Ajout d'un box d'une taille de 5 pixels
               const SizedBox(height: 5),
+              // Ajout d'un texte tout en majuscule avec une couleur rose
               Text("Tel".toUpperCase(),
               style: const TextStyle(color: Colors.pink),)
             ],
@@ -130,22 +154,34 @@ class PartieIcone extends StatelessWidget {
             
           ),
           Container(
+            // Ajout d'une marge intérieure de 20 pixel de tous les côtés au deuxième widget enfant 
             padding: const EdgeInsets.all(20),
+            // Ajout d'une colonne qui est le widget enfant du widget parent qui est le deuxième conteneur
             child: Column(
+              // Création de trois bloc de conteneur dans le widget parent
               children: [
+              // Ajout d'une icone d'email avec une couleur rose
               const Icon(Icons.mail, color: Colors.pink),
+              // Ajout d'un box d'une taille de 5 pixels
               const SizedBox(height: 5),
+              // Ajout d'un texte tout en majuscule avec une couleur rose
               Text("Tel".toUpperCase(),
               style: const TextStyle(color: Colors.pink),)
             ],
             ),
           ),
           Container(
+            // Ajout d'une marge intérieure de 20 pixel de tous les côtés au troisième widget enfant 
             padding: const EdgeInsets.all(20),
+            // Ajout d'une colonne qui est le widget enfant du widget parent qui est le troisième conteneur
             child: Column(
+              // Ajout de trois bloc de conteneur dans le widget parent
               children: [
+              // Ajout d'une icone de partage avec une couleur rose  
               const Icon(Icons.share, color: Colors.pink),
+              // Ajout d'un box d'une taille de 5 pixels
               const SizedBox(height: 5),
+              // Ajout d'un texte tout en majuscule avec une couleur rose
               Text("Partage".toUpperCase(),
               style: const TextStyle(color: Colors.pink),)
             ],
@@ -157,23 +193,38 @@ class PartieIcone extends StatelessWidget {
   }
 }
 
+/* Déclaration et initialisation de la classe qui va nous permettre de créer le troisième bloc de la page
+ La classe PartieRubrique est un autre Widget Stateless qui représente un bloc de type conteneur. 
+ Elle définit un conteneur contenant un element enfant de type ligne qui englobe deux elements enfants de type d'arrondi de cadre qui eux à leur tour contiennent chacun enfant de type image. */
 class PartieRubrique extends StatelessWidget {
   const PartieRubrique({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      // Ajout d'une marge interieur des côtés horizontaux seulement.
       padding: const EdgeInsets.symmetric(horizontal: 20),
+      // Ajout d'une ligne dans son widget parent
       child: Row(
+        // Alignement horizontal des widgets enfants
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // Ajout de deux cadres rectangulaire aux côtés arrondis dans le widget parent
         children: [
+          // Ajout d'un cadre rectangulaire aux côtés arrondis dans le widget parent
           ClipRRect(
+            // Ajout d'une bordure circulaire de 8 pixels aux extremités du cadre rectangulaire
             borderRadius: BorderRadius.circular(8),
-            child: const Image(image: AssetImage('assets/images/img4.jpg')),
+            // Ajout d'une image dans le cadre comme widget enfant
+            child: const Image(
+              image: AssetImage('assets/images/img4.jpg')),
           ),
+          // Ajout d'un cadre rectangulaire aux côtés arrondis dans le widget parent
           ClipRRect(
+            // Ajout d'une bordure circulaire de 8 pixels aux extremités du cadre rectangulaire
             borderRadius: BorderRadius.circular(8),
-            child: const Image(image: AssetImage('assets/images/img5.jpeg')),
+            // Ajout d'une image dans le cadre comme widget enfant
+            child: const Image(
+              image: AssetImage('assets/images/img5.jpeg')),
           ),
         ],
       ),
